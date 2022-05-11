f0216a1f6935f30d86a247391f13f5cf4d801ad3
Sasa-saska <99489644+Sasa-saska@users.noreply.github.com>
2022-05-11 18:13:29 +0300

    3

--- task02-slice.go
+++ task02-slice.go
@@ -1,13 +1,12 @@
 package homework
 
-import "sort"
-
 func reverse(input []int64) (result []int64) {
 	result = make([]int64, len(input))
 	copy(result, input)
-	sort.Slice(result, func(i, j int) bool {
-		return result[i] > result[j]
-	})
+	for i := len(result)/2 - 1; i >= 0; i-- {
+		opp := len(result) - 1 - i
+		result[i], result[opp] = result[opp], result[i]
+	}
 
 	return result
 }
